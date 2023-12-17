30cdf2c84ba2636d53442b8ed06df0eb83262c7a
Manjot Bal <manjot@manjotbal.ca>
2023-12-17 14:02:15 -0700

    Add glfw submodule 3.3 stable

--- main.cpp
+++ main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
 
 int main (int, char**)
 {
-	std::cout << "Hello world!" << std::endl;
+	glfwInit();
+
+	if (!glfwInit())
+	{
+		std::cerr << "Could not initialize GLFW" << std::endl;
+		return 1;
+	}
+
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "Learn WebGPU", nullptr, nullptr);
+	if (!window)
+	{
+		std::cerr << "Could not create window" << std::endl;
+		return 1;
+	}
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
