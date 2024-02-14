c708fff2efb3a25d1aab0f7c3da2f43b14454007
Manjot Bal <manjot@manjotbal.ca>
2024-02-13 21:34:09 -0700

    Initialize GLFW and WebGPU in their own functions.

--- main.cpp
+++ main.cpp
@@ -7,28 +7,49 @@
 
 #include "webgpu-utils.hpp"
 
+struct WGPUContext
+{
+	bool initialized;
 
-int main (int, char**)
+	WGPUInstance instance;
+	WGPUAdapter adapter;
+	WGPUDevice device;
+	WGPUSurface surface;
+
+	~WGPUContext()
+	{
+		wgpuDeviceRelease(device);
+		wgpuSurfaceRelease(surface);
+		wgpuAdapterRelease(adapter);
+		wgpuInstanceRelease(instance);
+	}
+};
+
+GLFWwindow* glfwInitialize()
 {
 	// Setup GLFW
-	glfwInit();
-
 	if (!glfwInit())
 	{
 		std::cerr << "Could not initialize GLFW" << std::endl;
-		return 1;
+		return nullptr;
 	}
 
 	// GLFW is unaware of WebGPU (at the moment) so do not setup any graphics API with it
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-	GLFWwindow* window = glfwCreateWindow(1280, 720, "Learn WebGPU", nullptr, nullptr);
-	if (!window)
+	GLFWwindow* pWindow = glfwCreateWindow(1280, 720, "Learn WebGPU", nullptr, nullptr);
+	if (!pWindow)
 	{
 		std::cerr << "Could not create window" << std::endl;
-		return 1;
+		return nullptr;
 	}
 
-	std::cout << "GLFW initialized successfully: " << window << std::endl;
+	return pWindow;
+}
+
+WGPUContext wgpuInitialize(GLFWwindow* pWindow)
+{
+	WGPUContext ctx;
+	ctx.initialized = false;
 
 	// Setup WebGPU
 #if defined(WEBGPU_BACKEND_DAWN)
@@ -43,38 +64,38 @@ int main (int, char**)
 
 	WGPUInstanceDescriptor desc = {};
 	desc.nextInChain = nullptr;
-	WGPUInstance instance = wgpuCreateInstance(&desc);
+	ctx.instance = wgpuCreateInstance(&desc);
 
-	if (!instance)
+	if (!ctx.instance)
 	{
 		std::cerr << "Could not initialize WebGPU" << std::endl;
-		return 1;
+		return ctx;
 	}
-	std::cout << "WebGPU initialized successfully: " << instance << std::endl;
+	std::cout << "WebGPU initialized successfully: " << ctx.instance << std::endl;
 
 	// Retrieving the surface is platform dependant, so use a helper function
-	WGPUSurface surface = glfwGetWGPUSurface(instance, window);
-	if (!surface)
+	ctx.surface = glfwGetWGPUSurface(ctx.instance, pWindow);
+	if (!ctx.surface)
 	{
-		std::cerr << "Could get surface" << std::endl;
-		return 1;
+		std::cerr << "Could not get surface" << std::endl;
+		return ctx;
 	}
 
 	// Retrieve the WebGPU adapter
 	WGPURequestAdapterOptions adapterOptions{};
 	adapterOptions.nextInChain = nullptr;
-	adapterOptions.compatibleSurface = surface;
+	adapterOptions.compatibleSurface = ctx.surface;
 
-	WGPUAdapter adapter = wgpuUtils::requestAdapter(instance, &adapterOptions);
-	if (!adapter)
+	ctx.adapter = wgpuUtils::requestAdapter(ctx.instance, &adapterOptions);
+	if (!ctx.adapter)
 	{
 		std::cerr << "Could not retrieve adapter" << std::endl;
-		return 1;
+		return ctx;
 	}
-	std::cout << "Got adapter: " << adapter << std::endl;
-	wgpuUtils::printAdapterFeatures(adapter);
+	std::cout << "Got adapter: " << ctx.adapter << std::endl;
+	wgpuUtils::printAdapterFeatures(ctx.adapter);
 
-	// Use adapter to and device description to retrieve a device
+	// Use adapter and device description to retrieve a device
 	WGPUDeviceDescriptor deviceDesc;
 	deviceDesc.nextInChain = nullptr;
 	deviceDesc.label = "My Device";
@@ -83,11 +104,11 @@ int main (int, char**)
 	deviceDesc.defaultQueue.nextInChain = nullptr;
 	deviceDesc.defaultQueue.label = "Default Queue";
 
-	WGPUDevice device = wgpuUtils::requestDevice(adapter, &deviceDesc);
-	if (!device)
+	ctx.device = wgpuUtils::requestDevice(ctx.adapter, &deviceDesc);
+	if (!ctx.device)
 	{
 		std::cerr << "Could not retrieve device" << std::endl;
-		return 1;
+		return ctx;
 	}
 
 	auto onDeviceError = [](WGPUErrorType type, char const* message, void* /*pUserData*/)
@@ -97,19 +118,37 @@ int main (int, char**)
 			std::cout << " (" << message << ")";
 		std::cout << std::endl;
 	};
-	wgpuDeviceSetUncapturedErrorCallback(device, onDeviceError, nullptr);
+	wgpuDeviceSetUncapturedErrorCallback(ctx.device, onDeviceError, nullptr);
 
-	while (!glfwWindowShouldClose(window))
+	ctx.initialized = true;
+	return ctx;
+}
+
+int main (int, char**)
+{
+	GLFWwindow* pWindow = glfwInitialize();
+	if (!pWindow)
+	{
+		std::cerr << "GLFW not initialized correctly" << std::endl;
+		return 1;
+	}
+	std::cout << "GLFW initialized successfully: " << pWindow << std::endl;
+
+	WGPUContext wgpuCtx = wgpuInitialize(pWindow);
+	if (!wgpuCtx.initialized)
+	{
+		std::cerr << "WebGPU not initialized correctly" << std::endl;
+		return 1;
+	}
+	std::cout << "WebGPU initialized successfully" << std::endl;
+
+	while (!glfwWindowShouldClose(pWindow))
 	{
 		glfwPollEvents();
 	}
 
 	// cleanup
-	wgpuDeviceRelease(device);
-	wgpuSurfaceRelease(surface);
-	wgpuAdapterRelease(adapter);
-	wgpuInstanceRelease(instance);
-	glfwDestroyWindow(window);
+	glfwDestroyWindow(pWindow);
 	glfwTerminate();
 	return 0;
 }
